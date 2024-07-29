1b6d7e4ca45c80aa0f0deb8b01c31de2e2f22c2f
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-07-29 11:59:35 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
-from flask import Flask, escape
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return "<h1>Python Operations with Flask Routing and Views</h1>"
+    return f'<h1>Python Operations with Flask Routing and Views</h1>'
 
 @app.route('/print/<path:parameter>')
 def print_string(parameter):
@@ -16,25 +15,26 @@ def print_string(parameter):
 
 @app.route('/count/<int:parameter>')
 def count(parameter):
-   num = f''
-   for n in range(parameter):
-      num += f'{n}\n'
-   return num
+    num = f''
+
+    for n in range(parameter):
+        num += f'{n}\n'
+    return num
 
 @app.route('/math/<int:num1>/<string:operation>/<int:num2>')
-def math(num1, operation, num2):
-    if operation == '+':
+def math(num1, num2, operation):
+    if operation == "+":
         return str(num1 + num2)
-    elif operation == '-':
+    elif operation == "-":
         return str(num1 - num2)
-    elif operation == '*':
+    elif operation == "*":
         return str(num1 * num2)
-    elif operation == 'div':
+    elif operation == "div":
         return str(num1 / num2)
-    elif operation == '%':
+    elif operation == "%":
         return str(num1 % num2)
     else:
-        return 'invalid'
+        return "Invalid"
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
